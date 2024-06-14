d1f5ac48fbcb7cf4c6199e47cb5844e7c0d91840
Peter Mugoya <petermugoya2@gmail.com>
2024-06-14 15:54:35 +0300

    change the jest.config.js to new setting

--- MedAssist-master/jest.config.js
+++ MedAssist-master/jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  preset: 'eact-native',
-  // Add any additional configurations here
-  // For example:
-   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  // transform: {...},
-  // moduleNameMapper: {...},
-  //... other config options...
+  preset: 'react-native',
 };
